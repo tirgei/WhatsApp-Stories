d001a12ebde229d824c923e5b82943d0ea899c1e
Vincent Tirgei <tirgeic@gmail.com>
2018-07-29 23:32:41 +0300

    updated stories adapter

--- app/src/main/java/com/gelostech/whatsappstories/adapters/StoriesAdapter.kt
+++ app/src/main/java/com/gelostech/whatsappstories/adapters/StoriesAdapter.kt
@@ -1,15 +1,17 @@
 package com.gelostech.whatsappstories.adapters
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.gelostech.whatsappstories.R
-import com.gelostech.whatsappstories.Story
 import com.gelostech.whatsappstories.callbacks.StoryCallback
-import com.gelostech.whatsappstories.commoners.K
-import com.gelostech.whatsappstories.databinding.ItemImageBinding
+import com.gelostech.whatsappstories.commoners.AppUtils.setDrawable
+import com.gelostech.whatsappstories.databinding.ItemStoryBinding
+import com.gelostech.whatsappstories.models.Story
 import com.gelostech.whatsappstories.utils.inflate
+import com.mikepenz.ionicons_typeface_library.Ionicons
 
-class StoriesAdapter(private val callback: StoryCallback) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class StoriesAdapter(private val callback: StoryCallback, private val context: Context) : RecyclerView.Adapter<StoriesAdapter.StoriesHolder>(){
     private val stories = mutableListOf<Story>()
 
     fun addStory(story: Story) {
@@ -26,43 +28,25 @@ class StoriesAdapter(private val callback: StoryCallback) : RecyclerView.Adapter
         stories.clear()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when(viewType) {
-            K.TYPE_IMAGE -> {
-                ImageHolder(parent.inflate(R.layout.item_image), callback)
-            }
-
-            else -> {
-                VideoHolder(parent.inflate(R.layout.item_image))
-            }
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesHolder {
+        return StoriesHolder(parent.inflate(R.layout.item_story), callback, context)
     }
 
     override fun getItemCount(): Int = stories.size
 
-    override fun getItemViewType(position: Int): Int {
-        return stories[position].type
+    override fun onBindViewHolder(holder: StoriesHolder, position: Int) {
+        holder.bind(stories[position])
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(holder) {
-            is ImageHolder -> holder.bind(stories[position])
-            is VideoHolder -> holder.bind(stories[position])
+    class StoriesHolder(private val binding: ItemStoryBinding, private val callback: StoryCallback, private val context: Context): RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.videoIcon.setImageDrawable(setDrawable(context, Ionicons.Icon.ion_videocamera, R.color.white, 35))
         }
-    }
-
-    class ImageHolder(private val binding: ItemImageBinding, private val callback: StoryCallback): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(story: Story) {
             binding.story = story
-        }
-
-    }
-
-    class VideoHolder(private val binding: ItemImageBinding): RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(story: Story) {
-            binding.story = story
+            binding.callback = callback
         }
 
     }
