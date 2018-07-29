eb4af18197bd5371ef485c09a30afd704937914e
Vincent Tirgei <tirgeic@gmail.com>
2018-07-29 23:55:32 +0300

    changed videos icon

--- app/src/main/java/com/gelostech/whatsappstories/adapters/StoriesAdapter.kt
+++ app/src/main/java/com/gelostech/whatsappstories/adapters/StoriesAdapter.kt
@@ -41,7 +41,7 @@ class StoriesAdapter(private val callback: StoryCallback, private val context: C
     class StoriesHolder(private val binding: ItemStoryBinding, private val callback: StoryCallback, private val context: Context): RecyclerView.ViewHolder(binding.root) {
 
         init {
-            binding.videoIcon.setImageDrawable(setDrawable(context, Ionicons.Icon.ion_videocamera, R.color.white, 35))
+            binding.videoIcon.setImageDrawable(setDrawable(context, Ionicons.Icon.ion_play, R.color.white, 27))
         }
 
         fun bind(story: Story) {
