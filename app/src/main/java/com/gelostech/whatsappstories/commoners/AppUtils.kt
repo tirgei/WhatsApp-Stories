ae8000cc76b5eb48af363f3ee1fbe5460ea8d9a9
Vincent Tirgei <tirgeic@gmail.com>
2018-07-29 23:33:25 +0300

    added function to return FontAwesome IIcon

--- app/src/main/java/com/gelostech/whatsappstories/commoners/AppUtils.kt
+++ app/src/main/java/com/gelostech/whatsappstories/commoners/AppUtils.kt
@@ -1,8 +1,17 @@
 package com.gelostech.whatsappstories.commoners
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
 import java.io.File
 
 object AppUtils {
+
+    fun setDrawable(context: Context, icon: IIcon, color: Int, size: Int): Drawable {
+        return IconicsDrawable(context).icon(icon).color(ContextCompat.getColor(context, color)).sizeDp(size)
+    }
 
     fun isImage(file: File): Boolean {
         val fileName = file.name.toLowerCase()
