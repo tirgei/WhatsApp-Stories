d7d07dc167ec7d501c78d0399da9e37f7c4f97b5
Vincent Tirgei <tirgeic@gmail.com>
2018-07-29 12:23:45 +0300

    moved to activities package

--- app/src/main/java/com/gelostech/whatsappstories/activities/MainActivity.kt
+++ app/src/main/java/com/gelostech/whatsappstories/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gelostech.whatsappstories
+package com.gelostech.whatsappstories.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gelostech.whatsappstories.R
 
 class MainActivity : AppCompatActivity() {
 
